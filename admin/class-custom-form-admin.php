e0073ddd95beaba8a91d5250a5244575e4562ce3
Ravina Vala <vala8460@gmail.com>
2023-01-27 05:41:04 +0530

    Set up menu for custom contact form settings

--- admin/class-custom-form-admin.php
+++ admin/class-custom-form-admin.php
@@ -26,7 +26,17 @@ class Custom_Form_Admin {
      * @param      string    $version    The version of this plugin.
      */
     public function __construct() {
-        
+        add_action('admin_menu', array($this, 'customform_admin_menu'));
     }
 
+    public function customform_admin_menu() {
+        add_menu_page('Custom Contact Form', 'Custom form settings', 'manage_options', 'custom_form_settings', array(
+            $this,
+            'custom_form_content',
+        ));
+    }
+
+    public function custom_form_content() {
+        require CUSTOM_FORM_ADMIN_HTML_PATH . 'custom-form-admin-page.php';
+    }
 }
